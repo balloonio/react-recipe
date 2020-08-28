0d010641035a5443325058731800604132698f2c
Bolun Zhang <rzhangbolun@gmail.com>
2020-08-28 17:17:32 -0400

    add recipe

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+import Recipe from "./Recipe";
+
+const App = () => {
+  // const [counter, setCounter] = useState(0);
+
+  // useEffect(() => {
+  //   console.log("Effect has been run.");
+  // }, []);
+
+  const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("");
+
+  useEffect(() => {
+    const APP_ID = "b2ffc676";
+    const APP_KEY = "be161522035dd64ccf6735f08d37b0b4";
+    const EXAMPLE_URL = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
+    const getRecipes = async () => {
+      if (query === "") {
+        return;
+      }
+      const response = await fetch(EXAMPLE_URL);
+      const data = await response.json();
+      console.log("We are fetching data and got " + EXAMPLE_URL);
+      console.log(data.hits);
+      setRecipes(data.hits);
+    };
+
+    getRecipes();
+  }, [query]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1></h1>
+      <form
+        className="search-form"
+        onSubmit={(e) => {
+          setQuery(search);
+          setSearch("");
+          e.preventDefault();
+        }}
+      >
+        <input
+          className="search-bar"
+          type="text"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search}
+        ></input>
+        <button className="search-btn" type="submit">
+          Search
+        </button>
+      </form>
+      <div className="recipes">
+        {recipes.map((recipe, index) => (
+          <Recipe
+            key={index}
+            title={recipe.recipe.label}
+            calories={recipe.recipe.calories}
+            image={recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+          />
+        ))}
+      </div>
+      {/* <button onClick={()=> setCounter(counter+1)}>{counter}</button> */}
     </div>
   );
-}
-
+};
 export default App;
